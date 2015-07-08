5fb0c42089bb07462a4118c6be0c533eb5c8afaa
JuanDMeGon <contacto@juandmegon.com>
2015-07-07 21:37:11 -0500

    Especificando el nombre de la tabla de profesores
    - Se especifica el nombre correcto de la tabla para el modelo Profesor.

--- app/Profesor.php
+++ app/Profesor.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profesor extends Model
 {
+	protected $table = 'profesores';
+
 	protected $fillable = ['nombre', 'direccion', 'telefono', 'profesion'];
 
 	protected $hidden = ['id', 'created_at', 'updated_at'];
